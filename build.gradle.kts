13217265a6ab4a075f8495919303ed1d722d4da1
Ankit Gupta <ankit.gupta@cricbuzz.com>
2023-07-03 16:56:43 +0530

    Fixed danger issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ subprojects {
         android.set(false)
         outputToConsole.set(true)
         outputColorName.set("RED")
-        ignoreFailures.set(true)
+        ignoreFailures.set(false)
         reporters {
             reporter(ReporterType.CHECKSTYLE)
             reporter(ReporterType.PLAIN_GROUP_BY_FILE)
